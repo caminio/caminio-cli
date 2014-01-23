e248d5568922e70cbce6e7bd147e813c04ff160d
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-01-24 00:45:03 +0100

    more complete controller generator

--- templates/generator/controller_tmpl.js
+++ templates/generator/controller_tmpl.js
@@ -6,7 +6,13 @@ module.exports = function __NAME__( caminio, policies, middleware ){
   return {
 
     _before: {
+      // set up your before filters here
+      // typical before filters are policies.ensureLogin or 
+      // policies.requireAdmin
+      //
       // e.g.: '*': policies.ensureLogin,
+      //       'index,show': [ policies.requireAdmin, myCustomCheck ]
+      //
     },
 
     /**
@@ -14,9 +20,12 @@ module.exports = function __NAME__( caminio, policies, middleware ){
      */
     'index': [
       function( req, res ){
-        res.send( req.text );
+        // this tries to find a template file in any of api/views directory
+        // named [controller_namespace]/<controller_name>/<action_name>
+        // here this would be __name__/index
+        res.caminio.render();
       }
-    ],
+    ]
 
   }
 
