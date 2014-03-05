8c73b29d8f47a730a9a957005a6abf976385a6b5
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-03-05 02:13:33 +0100

    mailer added to default project files

---
+++ templates/project/config/mailer.js
@@ -0,0 +1,20 @@
+module.exports.mailer = {
+  
+  from: 'camin.io <no-reply@tastenwerk.com>',
+
+  host: 'localhost',
+
+  port: 465,
+
+  secureConnection: true,
+  
+  auth: {
+    user: '',
+    pass: ''
+  },
+
+  support: 'no support address specified yet.',
+
+  goodbye: 'have a nice day,\n  caminio'
+
+};
